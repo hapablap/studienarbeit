2e1cf7415544aedd63649fffc022a96b1aa79c84
Thomas Klytta <thomas.klytta@gmail.com>
2014-06-05 19:20:16 +0200

    Worker Args class

---
+++ EEGETAnalysis.GUI/CSVWorkerArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEGETAnalysis.GUI
+{
+    public class CSVWorkerArgs
+    {
+        public string CsvFilePath { get; set; }
+    }
+}
